63f4d60a25b20fb01e6d57dda35e159648979b7c
Mason Turbyfill <masonsturbyfill61796@gmail.com>
2019-06-17 10:52:59 -0400

    Added sample get and post requests.

--- Code/Node/first-api/server.js
+++ Code/Node/first-api/server.js
@@ -1,13 +1,42 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extdended: false}));
+
+var ingredients = [
+    {
+        'id':'232kAk',
+        'text':'Eggs'
+    },
+    {
+        'id':'dkP345',
+        'text':'Milk'
+    },
+    {
+        'id':'dkcuu7',
+        'text':'Bacon'
+    },
+    {
+        'id':'73hdy',
+        'text':"Frog's Legs"
+    }
+];
 
 app.get('/', function(request, response) {
-    response.send('My first API');
+    response.send(ingredients);
 });
 
-app.get('/things', function(req, res) {
-    res.send('Things boiiiiii');
-});
+app.post('/', function(request, response) {
+    var ingredient = request.body;
+    if (!ingredient || ingredient.text == '') {
+        response.status(500).send({error: 'Your ingredient must have text'});
+    } else {
+        ingredients.push(ingredient);
+        response.status(200).send(ingredients);
+    }
+})
 
 app.listen(3000, function() {
     console.log('First API running on port 3000');
