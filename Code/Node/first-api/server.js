1bdcc4f948716d9df65526855f0b4130bf1755c4
Mason Turbyfill <masonsturbyfill61796@gmail.com>
2019-06-17 11:32:47 -0400

    Put in CRUD functions

--- Code/Node/first-api/server.js
+++ Code/Node/first-api/server.js
@@ -3,7 +3,7 @@ var app = express();
 var bodyParser = require('body-parser');
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extdended: false}));
+app.use(bodyParser.urlencoded({extended: false}));
 
 var ingredients = [
     {
@@ -24,19 +24,42 @@ var ingredients = [
     }
 ];
 
-app.get('/', function(request, response) {
+app.get('/ingredients', function(request, response) {
     response.send(ingredients);
 });
 
-app.post('/', function(request, response) {
+app.post('/ingredients', function(request, response) {
     var ingredient = request.body;
-    if (!ingredient || ingredient.text == '') {
+    if (!ingredient || ingredient.text === '') {
         response.status(500).send({error: 'Your ingredient must have text'});
     } else {
         ingredients.push(ingredient);
         response.status(200).send(ingredients);
     }
 })
+
+app.put('/ingredients/:ingredientId', function(request, response) {
+    var ingredientTitle = request.body.text;
+
+    if (!ingredientTitle || ingredientTitle === '') {
+        response.status(500).send({error: 'You must provide ingredient text'})
+    } else {
+        var objectFound = false;
+        for (var x = 0; x < ingredients.length; x++) {
+            var ing = ingredients[x];
+
+            if (ing.id === request.params.ingredientId) {
+                ingredients[x].text = ingredientTitle;
+                break;
+            }
+        }
+        if (!objectFound) {
+            response.status(500).send({error:'Ingredient ID not found'});
+        } else {
+            response.send(ingredients);
+        }
+    }
+});
 
 app.listen(3000, function() {
     console.log('First API running on port 3000');
